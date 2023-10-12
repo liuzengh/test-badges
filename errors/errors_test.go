1961ce20cdd8020d82f23268b32926d5f436ef1b
amdahliu <amdahliu@tencent.com>
2023-10-12 16:47:19 +0800

    remove an ut

--- errors/errors_test.go
+++ errors/errors_test.go
@@ -10,22 +10,6 @@ import (
 	"github.com/liuzengh/test-badges/errors"
 )
 
-func TestNewEqual(t *testing.T) {
-	// Different allocations should not be equal.
-	if errors.New("abc") == errors.New("abc") {
-		t.Errorf(`New("abc") == New("abc")`)
-	}
-	if errors.New("abc") == errors.New("xyz") {
-		t.Errorf(`New("abc") == New("xyz")`)
-	}
-
-	// Same allocation should be equal to itself (not crash).
-	err := errors.New("jkl")
-	if err != err {
-		t.Errorf(`err != err`)
-	}
-}
-
 func TestErrorMethod(t *testing.T) {
 	err := errors.New("abc")
 	if err.Error() != "abc" {
